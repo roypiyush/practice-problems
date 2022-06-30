34d34c74c83fc0c5a30b9675b34ecce800d8b63c
Piyush Roy <piyush2k13@gmail.com>
2022-06-30 19:08:48 +0530

    added more programs

--- src/main/java/com/lib/Triplets.java
+++ src/main/java/com/lib/Triplets.java
@@ -18,4 +18,24 @@ public class Triplets<L, R, V> {
     public String toString() {
         return String.format("%s, %s -> %s", left.toString(), right.toString(), value.toString());
     }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final Triplets<?, ?, ?> triplets = (Triplets<?, ?, ?>) o;
+
+        if (left != null ? !left.equals(triplets.left) : triplets.left != null) return false;
+        if (right != null ? !right.equals(triplets.right) : triplets.right != null) return false;
+        return value != null ? value.equals(triplets.value) : triplets.value == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = left != null ? left.hashCode() : 0;
+        result = 31 * result + (right != null ? right.hashCode() : 0);
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        return result;
+    }
 }
