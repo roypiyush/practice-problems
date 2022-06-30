a76a0ae8bc51cd5662b238916587ea73b43303df
Piyush Roy <piyush2k13@gmail.com>
2022-06-30 08:41:47 +0530

    largest Rectange

--- src/main/java/com/personal/LargestRectHist.java
+++ src/main/java/com/personal/LargestRectHist.java
@@ -1,7 +1,11 @@
 package com.personal;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class LargestRectHist {
-    public static int largestRectangleN2(final int[] hists) {
+
+    private static int bruteForce(final int[] hists) {
         int result = 0;
         for (int i = 1; i < hists.length - 1; i++) {
             int left = i;
@@ -17,8 +21,38 @@ public class LargestRectHist {
         return result;
     }
 
+    private static int linearTime(final int[] hists) {
+
+        int curPtr = 0;
+        int maxArea = 0;
+        Deque<Integer> stack = new LinkedList<>();
+
+        while (curPtr < hists.length) {
+            if (stack.size() == 0 || hists[curPtr] > hists[stack.peek()]) {
+                stack.push(curPtr);
+                curPtr++;
+            } else {
+                final Integer h = stack.pop();
+                int area = (stack.peek() == null ? curPtr : curPtr - stack.peek() - 1) * hists[h];
+                maxArea = Math.max(maxArea, area);
+            }
+        }
+        while (stack.peek() != null) {
+            final Integer h = stack.pop();
+            int area = (stack.peek() == null ? curPtr : curPtr - stack.peek() - 1) * hists[h];
+            maxArea = Math.max(maxArea, area);
+        }
+        return maxArea;
+    }
+
     public static void main(String[] args) {
-        final int[] hists = {6, 2, 5, 4, 5, 1, 6};
-        System.out.println(largestRectangleN2(hists));
+        int[] hists;
+        hists = new int[]{6, 2, 5, 4, 5, 1, 6};
+        System.out.println(bruteForce(hists));
+        System.out.println(linearTime(hists));
+
+        hists = new int[]{2, 3, 2, 4, 0, 5, 3, 4};
+        System.out.println(bruteForce(hists));
+        System.out.println(linearTime(hists));
     }
 }
