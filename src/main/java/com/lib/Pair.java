5807525fceb1a75ae8e8d79fd07c4df9aaf59e61
Piyush Roy <piyushroy@users.noreply.github.com>
2022-06-30 09:36:48 +0530

    added lib

---
+++ src/main/java/com/lib/Pair.java
@@ -0,0 +1,19 @@
+package com.lib;
+
+public class Pair<L, R> {
+    L left;
+    R right;
+
+    private Pair(L left, R right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public static <L, R> Pair of(L left, R right) {
+        return new Pair<L, R>(left, right);
+    }
+
+    public String toString() {
+        return String.format("%s -> %s", left.toString(), right.toString());
+    }
+}
