6bc5976dcdb7a5fe72bca749f37a16ee331e58cc
Piyush Roy <piyushroy@users.noreply.github.com>
2022-07-02 11:42:39 +0530

    added counting, move_array, palindromic number

---
+++ src/main/java/com/personal/CountingSort.java
@@ -0,0 +1,34 @@
+package com.personal;
+
+import java.util.Arrays;
+
+public class CountingSort {
+    public static void main(String[] args) {
+        int[] arr = {8, 6, 3, 1, 5, 7, 2, 7, 2, 1, 6, 7, 2, 9, 1, 0, 0};
+        linearSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void linearSort(final int[] arr) {
+        int max = 9; // already known
+        int[] countingArray = new int[max + 1];
+        for (int i = 0; i < arr.length; i++) {
+            countingArray[arr[i]] += 1;
+        }
+
+        // cumulative frequencies
+        for (int i = 1; i < countingArray.length; i++) {
+            countingArray[i] = countingArray[i] + countingArray[i - 1];
+        }
+
+        int sortedArray[] = new int[arr.length];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            sortedArray[countingArray[arr[i]] - 1] = arr[i];
+            countingArray[arr[i]] -= 1;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sortedArray[i];
+        }
+    }
+}
