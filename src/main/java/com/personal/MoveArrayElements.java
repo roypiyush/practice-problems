0024689f7b5373d9b5fa38dce3b1aeb362206b77
Piyush Roy <piyush2k13@gmail.com>
2022-07-02 11:42:39 +0530

    added counting, move_array, palindromic number

---
+++ src/main/java/com/personal/MoveArrayElements.java
@@ -0,0 +1,51 @@
+package com.personal;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class MoveArrayElements {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        for (int i = 1; i <= arr.length; i++) {
+            System.out.printf("\nMove by %d\n", i);
+            System.out.println(Arrays.toString(arr));
+            rotateBy(arr, i);
+            System.out.println(Arrays.toString(arr));
+            // rotate back to original
+            rotateBy(arr, arr.length - i);
+        }
+    }
+
+    private static void rotateBy(final int[] arr, final int x) {
+        if (x % arr.length == 0) {
+            return;
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int a = 0; a < arr.length; a++) {
+            pq.add(a);
+        }
+
+        int d = x % arr.length;
+        int i = 0;
+        while (i < arr.length) {
+            int t = arr[i];
+            pq.remove(i);
+
+            int j = (i + d) % arr.length;
+            while (j != i) {
+                int v = arr[j];
+                pq.remove(j);
+
+                arr[j] = t;
+                t = v;
+                j = (j + d) % arr.length;
+            }
+            arr[j] = t;
+
+            if (pq.peek() == null) {
+                break;
+            }
+            i = pq.poll();
+        }
+    }
+}
--- src/main/java/com/personal/PalindromicNumber.java
+++ src/main/java/com/personal/PalindromicNumber.java
@@ -1,2 +1,33 @@
-package com.personal;public class PalindromicNumber {
+package com.personal;
+
+public class PalindromicNumber {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100000000; i++) {
+            if (isPalindromicNumber(i)) {
+                System.out.printf("%s ", i);
+            }
+        }
+    }
+
+    private static boolean isPalindromicNumber(final int number) {
+        if (number < 0) {
+            return false;
+        }
+
+        int result = 0;
+        int length = 0;
+        int t = number;
+        while (t / 10 != 0) {
+            length++;
+            t = t / 10;
+        }
+
+        t = number;
+        for (int power = length; power >= 0; power--) {
+            int mod = t % 10;
+            t = t / 10;
+            result += Math.pow(10, power) *  mod;
+        }
+        return result == number;
+    }
 }
