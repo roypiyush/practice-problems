74ce845a978c4fc12c85cfdfb7970b217f615b0f
Piyush Roy <piyushroy@users.noreply.github.com>
2022-08-20 01:58:21 +0530

    refactor

--- src/main/java/com/personal/ds/FenwickTree.java
+++ src/main/java/com/personal/ds/FenwickTree.java
@@ -6,51 +6,61 @@ import java.util.Arrays;
  * Binary Indexed Tree
  */
 public class FenwickTree {
-    private int tree[];
+    private final int[] tree;
 
-    void constructBIT(int[] arr) {
-        tree = new int[arr.length + 1];
-        int i = 1;
-        while (i < arr.length + 1) {
-            tree[i] = arr[i - 1];
-            i++;
+    public FenwickTree(final int[] arr) {
+        tree = Arrays.copyOf(arr, arr.length + 1);
+        // adjust
+        int i = arr.length;
+        while (i > 0) {
+            tree[i] = tree[i - 1];
+            i--;
         }
 
-        for (i = 1; i < arr.length + 1; i++) {
-            int j = i + (i & -i);
-            if (j <= arr.length) {
-                tree[j] += tree[i];
+        tree[i] = 0;
+        makeTree();
+    }
+
+    private void makeTree() {
+        for (int i = 1; i < tree.length; i++) {
+            int p = i + (i & -i); // parent
+            if (p < tree.length) {
+                tree[p] += tree[i];
             }
         }
     }
 
-    void update(int value, int i) {
-        i += 1;
-        while (i < tree.length) {
-            tree[i] += value;
-            i = i + (i & -i);
+    private void update(final int value, final int i) {
+        int cur = i + 1;
+        tree[cur] += value;
+
+        while (cur < tree.length) {
+            // keep updating parents
+            final int p = cur + (cur & -cur);
+            if (p < tree.length) {
+                tree[p] += tree[i];
+            }
+            cur = p;
         }
     }
 
-    int query(int i) {
-        i += 1;
+    private int query(final int i) {
+        int cur = i + 1;
         int sum = 0;
-        while (i > 0) {
-            sum += tree[i];
-            i = i - (i & -i);
+        while (cur > 0) {
+            sum += tree[cur];
+            cur -= cur & -cur;
         }
         return sum;
     }
 
-    int rangeQuery(int i, int j) {
+    private int rangeQuery(final int i, final int j) {
         return query(j) - query(i - 1);
     }
 
-
     public static void main(String[] args) {
-        int arr[] = {2, -1, 3, 1, 4};
-        FenwickTree fenwickTree = new FenwickTree();
-        fenwickTree.constructBIT(arr);
+        int[] arr = {2, -1, 3, 1, 4};
+        final FenwickTree fenwickTree = new FenwickTree(arr);
         System.out.println(Arrays.toString(arr));
         for (int i = 0; i < arr.length; i++) {
             System.out.printf("%d, ", fenwickTree.query(i));
@@ -58,12 +68,12 @@ public class FenwickTree {
         System.out.println();
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j < arr.length; j++) {
-                System.out.printf("Range query %d-%d is %d\n", i, j, fenwickTree.rangeQuery(i, j));
+                System.out.printf("Range query [%d, %d] is %d%n", i, j, fenwickTree.rangeQuery(i, j));
             }
         }
         fenwickTree.update(2, 3);
         for (int i = 0; i < arr.length; i++) {
-            System.out.printf("%d, ", fenwickTree.query(i));
+            System.out.printf("Sum [0, %d] is %d%n", i, fenwickTree.query(i));
         }
     }
 }
